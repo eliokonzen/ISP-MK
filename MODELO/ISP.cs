598323a5537ddc97078c4f058f33fe3805dc5772
francojalignier <francojalignier@gmail.com>
2017-10-16 17:50:57 -0300

    SINGLETON

---
+++ MODELO/ISP.cs
@@ -0,0 +1,25 @@
+﻿namespace MODELO
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    public class ISP : DbContext
+    {
+        public static ISP instancia;
+        public static ISP Obtener_instancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new ISP();
+            }
+            return instancia;
+        }
+
+        public ISP()
+
+            : base("name=Empresa")
+        {
+        }
+
+    }
+}
